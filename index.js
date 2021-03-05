a09d68741f25f2655d7b88eb6cfedba0cf647db3
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-04 19:16:21 -0500

    Import flashcards into index.js file.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectDB = require("./startup/db");
 const express = require("express");
 const app = express();
+const flashcards = require("./routes/flashcards");
 
 connectDB();
 
 app.use(express.json());
+app.use("/api/flashcards", flashcards);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
