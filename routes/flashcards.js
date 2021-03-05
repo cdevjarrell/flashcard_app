8409f1861fc8a765b3a13705d73cc5cacc6b348f
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-04 20:30:07 -0500

    Create GET route to search by flashcard id.

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -29,4 +29,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const flashcard = await Flashcard.findById(req.params.id);
+
+    if (!flashcard)
+      return res
+        .status(400)
+        .send(`The flashcard with id "${req.params.id} does not exist.`);
+
+    return res.send(flashcard);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
 module.exports = router;
