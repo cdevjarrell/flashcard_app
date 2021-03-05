fe70d89b077e2d2765be6fc17a79892cdaa9cf96
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-04 21:50:12 -0500

    Create collectionSchema for card collection.

---
+++ models/collection.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { flashcardSchema } = require("flashcard");
+
+const collectionSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    collectionStack: {type: [flashcardSchema], default:[]};
+});
+
+const Collection = mongoose.model("Collection", collectionSchema);
+
+function validateCollection(collection) {
+    const schema = Joi.object({
+        name: Joi.string().require(),
+    });
+    return schema.validate(collection);
+}
+
+exports.Collection = Collection;
+exports.validate = validateCollection;
