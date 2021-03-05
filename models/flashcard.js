35e3d8255d356face4e8255ebcf6c56a7cd54ed5
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-04 19:04:54 -0500

    Create flashcardSchema.

---
+++ models/flashcard.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const flashcardSchema = new mongoose.Schema({
+  category: { type: String, required: true, minlength: 2, maxlength: 255 },
+  description: { type: String, required: true },
+  dateModified: { type: Date, default: Date.now },
+});
