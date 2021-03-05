cdb757b0b4debe20de1ff50ba5d98a049dffa1db
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-04 19:47:04 -0500

    Create post functionality for new flashcard.

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -1,7 +1,21 @@
-const Flashcard = require('../models/flashcard');
-const express = require('express');
+const Flashcard = require("../models/flashcard");
+const express = require("express");
 const router = express.Router();
 
-//All endpoints and route handlers go here
+router.post("/", async (req, res) => {
+  try {
+    const flashcard = new Flashcard({
+      name: "Key of C",
+      description:
+        "The Key of C includes the notes 'C', 'D', 'E', 'F', 'G', 'A', and 'B'.",
+    });
+
+    await flashcard.save();
+
+    return res.send(flashcard);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 
 module.exports = router;
