7da26e2e3e1c667e7204aee3ae39b98bc647b896
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-04 20:03:48 -0500

    Add validation to post router.

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -1,12 +1,15 @@
-const Flashcard = require("../models/flashcard");
+const { Flashcard, validate } = require("../models/flashcard");
 const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error);
+
     const flashcard = new Flashcard({
       name: req.body.name,
-      description:req.body.description,
+      description: req.body.description,
     });
 
     await flashcard.save();
